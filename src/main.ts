f6b7a585275bb62200c8b7439e951b94b2fbb302
Jesus Berdugo <berdugopertuz@gmail.com>
2024-04-29 16:22:38 -0500

    feat: add table

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGoodTablePlugin from 'vue-good-table-next';
 
 import App from './App.vue'
 import router from './router'
 
 import '@/assets/index.css'
+import '@/assets/custom_vue_good_table.css'
+import 'vue-good-table-next/dist/vue-good-table-next.css'
 
 const app = createApp(App)
 
+app.use(VueGoodTablePlugin)
 app.use(createPinia())
 app.use(router)
 
