e292d7587297571f68ef033ea611ac33ffe3400f
Jesus Berdugo <berdugopertuz@gmail.com>
2024-04-29 10:30:15 -0500

    chore: add shadcn

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,14 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button'
+
+const counter = ref(0)
 </script>
 
 <template>
   <div>
     <h1>Hello World!</h1>
+    <Button @click="counter++">send</Button>
+    <span>Counter: {{ counter }}</span>
   </div>
 </template>
