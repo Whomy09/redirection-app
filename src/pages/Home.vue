8275750d385afd5865cc7edffd6a8d23e397e253
Jesus Berdugo <berdugopertuz@gmail.com>
2024-04-29 10:55:02 -0500

    chore: set font in the project

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button'
+
 
 const counter = ref(0)
 </script>
 
 <template>
-  <div>
-    <h1>Hello World!</h1>
-    <Button @click="counter++">send</Button>
-    <span>Counter: {{ counter }}</span>
+  <div class="max-w-4xl mx-auto">
+    <Card>
+      <h1>Hello World!</h1>
+      <Button @click="counter++">send</Button>
+      <span>Counter: {{ counter }}</span>
+    </Card>
   </div>
 </template>
