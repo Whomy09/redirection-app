25efa0a44eccc04fb4d8e235bbf61c7e5f2451c0
Jesus Berdugo <berdugopertuz@gmail.com>
2024-05-02 09:18:12 -0500

    chore: add logout

--- src/components/layouts/navigation/Sidebar.vue
+++ src/components/layouts/navigation/Sidebar.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
 import { menus } from '@/constants/menus'
+import { useRoute, useRouter } from 'vue-router'
+import { useUserSession } from '@/stores/userSession'
+import Button from '@/components/ui/button/Button.vue'
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent
+} from '@/components/ui/dropdown-menu'
 
 const route = useRoute()
+const router = useRouter()
+
+const userSessionStore = useUserSession()
+const { user } = storeToRefs(userSessionStore)
 
 function getClass(name: string) {
   const isActive = route.name === name
@@ -10,37 +23,54 @@ function getClass(name: string) {
   const activeClass = 'bg-gray-100'
   return isActive ? activeClass : ''
 }
+
+function handleLogout() {
+  userSessionStore.logout()
+  router.push({ name: 'login' })
+}
 </script>
 
 <template>
-  <div>
-    <div
-      class="hidden border border-slate-50 shadow p-6 lg:flex lg:flex-col lg:justify-between lg:w-[300px]"
-    >
-      <div>
-        <div class="flex items-center gap-4">
-          <i class="fa-solid fa-arrows-up-down-left-right text-xl"></i>
-          <h2 class="text-xl font-bold">Redirection App</h2>
-        </div>
-
-        <div class="flex flex-col gap-4 mt-8">
-          <router-link
-            v-for="{ name, icon, label } in menus"
-            :key="name"
-            class="p-2 hover:bg-gray-100 rounded-md transition ease-in"
-            :class="getClass(name)"
-            :to="{ name }"
-          >
-            <i :class="icon" class="mr-3" />
-            <span>{{ label }}</span>
-          </router-link>
-        </div>
+  <div
+    class="hidden border border-slate-50 shadow p-6 lg:flex lg:flex-col lg:justify-between lg:w-[300px]"
+  >
+    <div>
+      <div class="flex items-center gap-4">
+        <i class="fa-solid fa-arrows-up-down-left-right text-xl"></i>
+        <h2 class="text-xl font-bold">Redirection App</h2>
       </div>
 
+      <div class="flex flex-col gap-4 mt-8">
+        <router-link
+          v-for="{ name, icon, label } in menus"
+          :key="name"
+          class="p-2 hover:bg-gray-100 rounded-md transition ease-in"
+          :class="getClass(name)"
+          :to="{ name }"
+        >
+          <i :class="icon" class="mr-3" />
+          <span>{{ label }}</span>
+        </router-link>
+      </div>
+    </div>
+
+    <div class="flex justify-between items-center gap-4">
       <div class="flex items-center gap-4">
         <div class="w-[40px] h-[40px] rounded-full bg-gray-200"></div>
-        Jesús Berdugo
+        <span>{{ user.name }}</span>
       </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <i class="fa-solid fa-ellipsis"></i>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuItem class="focus:bg-transparent justify-center">
+            <Button class="bg-red-500 hover:bg-red-600" @click="handleLogout">Logout</Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   </div>
 </template>
