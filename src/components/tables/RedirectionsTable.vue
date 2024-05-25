796b25263428ced74192c009d0cfa2b5da6d1cc7
Jesus Berdugo <berdugopertuz@gmail.com>
2024-05-25 06:49:20 -0500

    feat: add tooltip

--- src/components/tables/RedirectionsTable.vue
+++ src/components/tables/RedirectionsTable.vue
@@ -3,6 +3,7 @@ import { formatDate } from '@/helpers'
 import StatusLabel from '../base/StatusLabel.vue'
 import type { IRedirection } from '@/types/redirection'
 import { columnsRedirectionTable as columns } from '@/constants/columns'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 defineProps<{
   rows: IRedirection[]
@@ -41,9 +42,18 @@ defineProps<{
           <StatusLabel :status="props.row.status" />
         </div>
         <div v-if="props.column.field === 'actions'">
-          <router-link :to="{ name: 'redirections-detail', params: { id: props.row.id } }">
-            <i class="fa-regular fa-clipboard text-xl hover:cursor-pointer" />
-          </router-link>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger as-child>
+                <router-link :to="{ name: 'redirections-detail', params: { id: props.row.id } }">
+                  <i class="fa-regular fa-clipboard text-xl hover:cursor-pointer" />
+                </router-link>
+              </TooltipTrigger>
+              <TooltipContent class="bg-gray-900 text-white">
+                <p>See details</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </template>
 
