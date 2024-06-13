fc094b191f1e5853c7e9dabcb142b3014bf44c30
Jesus Berdugo <berdugopertuz@gmail.com>
2024-06-13 18:38:20 -0500

    chore: add icon whatsapp

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -7,13 +7,23 @@ import Sidebar from './navigation/Sidebar.vue'
   <div class="h-screen flex flex-col lg:flex-row">
     <!-- Mobile -->
     <Navbar />
-    
+
     <!-- Desktop -->
     <Sidebar />
-    
+
     <!-- Content -->
-    <div class="w-full p-4 lg:py-6 lg:px-16 h-full">
+    <div class="relative w-full p-4 lg:py-6 lg:px-16 h-full">
       <slot />
+
+      <!-- WhatsApp Icon -->
+      <div class="absolute bottom-4 right-4 lg:bottom-6 lg:right-6">
+        <a class="flex items-center gap-2 animate-bounce" href="https://wa.me/573237220514" target="_blank">
+          <span class="font-bold text-xl text-green-500 hover:text-green-600">¡Ayuda!</span>
+          <i
+            class="fa-brands fa-square-whatsapp text-[64px] text-green-500 hover:text-green-600 transition-colors duration-300"
+          />
+        </a>
+      </div>
     </div>
   </div>
 </template>
